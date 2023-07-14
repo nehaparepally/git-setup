ee292e012ae55baea6f6ad00ce1ea0872107b405
neha <nehaparepally@gmail.com>
2023-07-14 10:22:35 -0400

    Initial Commit

---
+++ git setup/git_setupApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_setupApp.swift
+//  git setup
+//
+//  Created by Neha Parepally on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct git_setupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
